5d73683f47aafbbe5f2c953d85650c764ce3c88f
1HassanAbdi <hassanabdigaleb@gmail.com>
2023-10-11 07:39:39 -0400

    1er commit Tic Tac too

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Tictactoe from './component/tictactoe';
+
+function App() {
+  return (
+    <div className="App">
+      <Tictactoe></Tictactoe>
+      
+    </div>
+  );
+}
+
+export default App;
